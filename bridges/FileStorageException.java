c19f46015ee21936c7e6981a31efe572ece36859
surya-vamsi2310 <59705196+surya-vamsi2310@users.noreply.github.com>
2022-05-18 09:34:16 +0530

    Add files via upload

---
+++ bridges/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.app.test.login.controller;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
